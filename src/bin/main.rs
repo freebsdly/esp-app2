9f989d65f90717d75b73bdf59456a2d203149d1b
qinhuajun <qinhuajun@outlook.com>
2025-10-13 22:36:38 +0800

    use interrupt handle boot button

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,20 +6,25 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use defmt::info;
+use core::cell::RefCell;
+use critical_section::Mutex;
+use defmt::{info, warn};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::timer::timg::TimerGroup;
 use {esp_backtrace as _, esp_println as _};
-use esp_hal::gpio::{Output, OutputConfig, Level, Input, InputConfig};
+use esp_hal::gpio::{Output, OutputConfig, Level, Input, InputConfig, Event, Io};
+use esp_hal::{handler, ram};
 
 extern crate alloc;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
+
+static BOOT_BUTTON: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
@@ -42,12 +47,19 @@ async fn main(spawner: Spawner) {
     let (mut _wifi_controller, _interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
         .expect("Failed to initialize WIFI controller");
 
+    let mut io = Io::new(peripherals.IO_MUX);
+    io.set_interrupt_handler(handler);
+
     // 分配 GPIO 引脚
     let led = Output::new(peripherals.GPIO1, Level::Low, OutputConfig::default());
-    let boot_button = Input::new(peripherals.GPIO0, InputConfig::default());
+    let mut boot_button = Input::new(peripherals.GPIO0, InputConfig::default());
+
+    critical_section::with(|cs| {
+        boot_button.listen(Event::FallingEdge);
+        BOOT_BUTTON.borrow_ref_mut(cs).replace(boot_button)
+    });
 
     spawner.spawn(run()).expect("run spawn failed");
-    spawner.spawn(button_task(boot_button, led)).expect("button_task spawn failed");
 }
 
 #[embassy_executor::task]
@@ -76,4 +88,33 @@ async fn button_task(boot_button: Input<'static>, mut led: Output<'static>) {
         last_button_state = current_button_state;
         Timer::after(Duration::from_millis(10)).await;
     }
+}
+
+#[handler]
+#[ram]
+fn handler() {
+    info!(
+        "GPIO Interrupt with priority {}",
+        esp_hal::xtensa_lx::interrupt::get_level()
+    );
+
+    if critical_section::with(|cs| {
+        BOOT_BUTTON
+            .borrow_ref_mut(cs)
+            .as_mut()
+            .unwrap()
+            .is_interrupt_set()
+    }) {
+        info!("boot button was the source of the interrupt");
+    } else {
+        warn!("Button was not the source of the interrupt");
+    }
+
+    critical_section::with(|cs| {
+        BOOT_BUTTON
+            .borrow_ref_mut(cs)
+            .as_mut()
+            .unwrap()
+            .clear_interrupt()
+    });
 }
